bd3ffcf6b81a315169e1a74990f074ef3d40e119
Anuken <arnukren@gmail.com>
2019-02-23 22:30:58 -0500

    Moved incorrectly placed IntIntMap constructor

--- arc-core/src/io/anuke/arc/collection/IntIntMap.java
+++ arc-core/src/io/anuke/arc/collection/IntIntMap.java
@@ -87,6 +87,14 @@ public class IntIntMap implements Iterable<IntIntMap.Entry>{
         hasZeroValue = map.hasZeroValue;
     }
 
+    public static IntIntMap with(int... values){
+        IntIntMap map = new IntIntMap();
+        for(int i = 0; i < values.length; i += 2){
+            map.put(values[i], values[i + 1]);
+        }
+        return map;
+    }
+
     public void put(int key, int value){
         if(key == 0){
             zeroValue = value;
