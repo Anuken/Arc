73e3366c6b3a54e9f46ad6b57323e45d1d47963d
Anuken <arnukren@gmail.com>
2019-07-11 14:48:04 -0400

    Fixed potential freeze

--- backends/backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSNet.java
+++ backends/backend-robovm/src/com/badlogic/gdx/backends/iosrobovm/IOSNet.java
@@ -3,6 +3,7 @@ package com.badlogic.gdx.backends.iosrobovm;
 import io.anuke.arc.Net;
 import io.anuke.arc.function.Consumer;
 import io.anuke.arc.util.NetJavaImpl;
+import org.robovm.apple.dispatch.DispatchQueue;
 import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIApplication;
 import org.robovm.apple.uikit.UIApplicationOpenURLOptions;
@@ -24,8 +25,15 @@ public class IOSNet implements Net{
     public boolean openURI(String URI){
         NSURL url = new NSURL(URI);
         if(uiApp.canOpenURL(url)){
-            uiApp.openURL(url, new UIApplicationOpenURLOptions(), null);
-            return true;
+            try{
+                DispatchQueue.getMainQueue().async(() -> {
+                    uiApp.openURL(url, new UIApplicationOpenURLOptions(), null);
+                });
+                return true;
+            }catch(Throwable t){
+                t.printStackTrace();
+                return false;
+            }
         }
         return false;
     }
