c4556cc2ecff581d9c0e06f826bc10e7cacbabd0
Anuken <arnukren@gmail.com>
2019-10-07 20:26:08 -0400

    Delete old requests, pause building

--- arc-core/src/io/anuke/arc/collection/Queue.java
+++ arc-core/src/io/anuke/arc/collection/Queue.java
@@ -201,12 +201,17 @@ public class Queue<T> implements Iterable<T>{
         return -1;
     }
 
+    public boolean remove(T value){
+        return remove(value, false);
+    }
+
+
     /**
      * Removes the first instance of the specified value in the queue.
      * @param identity If true, == comparison will be used. If false, .equals() comparison will be used.
      * @return true if value was found and removed, false otherwise
      */
-    public boolean removeValue(T value, boolean identity){
+    public boolean remove(T value, boolean identity){
         int index = indexOf(value, identity);
         if(index == -1) return false;
         removeIndex(index);
