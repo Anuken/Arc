50a449a1e0eba80c1a448ed656102a169fea625b
Anuken <arnukren@gmail.com>
2019-11-04 22:27:51 -0500

    Fix selection

--- backends/backend-sdl/src/main/java/io/anuke/arc/backends/sdl/SdlNet.java
+++ backends/backend-sdl/src/main/java/io/anuke/arc/backends/sdl/SdlNet.java
@@ -42,7 +42,7 @@ public class SdlNet implements Net{
     public boolean openFolder(String file){
         try{
             if(OS.isWindows){
-                exec("explorer.exe " + file);
+                exec("explorer.exe /select," + file);
                 return true;
             }else if(OS.isLinux){
                 exec("xdg-open " + file);
