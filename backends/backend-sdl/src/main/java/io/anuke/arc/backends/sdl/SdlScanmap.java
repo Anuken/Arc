dc675c9bebeda4ebeb3d6e98593c91911ed40fd2
Joshua Fan <joshuaptfan@gmail.com>
2019-11-10 10:34:04 -0800

    Add unmapped SDL keyboard scancodes

--- backends/backend-sdl/src/main/java/io/anuke/arc/backends/sdl/SdlScanmap.java
+++ backends/backend-sdl/src/main/java/io/anuke/arc/backends/sdl/SdlScanmap.java
@@ -18,8 +18,11 @@ public class SdlScanmap{
             case 133: return KeyCode.COMMA;
             case 45:
             case 86: return KeyCode.MINUS;
-            case 55: return KeyCode.PERIOD;
-            case 56: return KeyCode.SLASH;
+            case 55:
+            case 99: return KeyCode.PERIOD;
+            case 56:
+            case 84: return KeyCode.SLASH;
+            case 85: return KeyCode.ASTERISK;
             case 98: return KeyCode.NUMPAD_0;
             case 89: return KeyCode.NUMPAD_1;
             case 90: return KeyCode.NUMPAD_2;
@@ -42,8 +45,11 @@ public class SdlScanmap{
             case 38: return KeyCode.NUM_9;
             case 203: return KeyCode.COLON;
             case 51: return KeyCode.SEMICOLON;
+            case 52: return KeyCode.APOSTROPHE;
             case 46: return KeyCode.EQUALS;
             case 206: return KeyCode.AT;
+            case 47: return KeyCode.LEFT_BRACKET;
+            case 48: return KeyCode.RIGHT_BRACKET;
             case 49: return KeyCode.BACKSLASH;
             case 53: return KeyCode.BACKTICK;
             case 4: return KeyCode.A;
@@ -72,11 +78,11 @@ public class SdlScanmap{
             case 27: return KeyCode.X;
             case 28: return KeyCode.Y;
             case 29: return KeyCode.Z;
-            case 76: return KeyCode.DEL;
             case 224: return KeyCode.CONTROL_LEFT;
             case 228: return KeyCode.CONTROL_RIGHT;
             case 225: return KeyCode.SHIFT_LEFT;
             case 229: return KeyCode.SHIFT_RIGHT;
+            case 57: return KeyCode.CAPS_LOCK;
             case 58: return KeyCode.F1;
             case 59: return KeyCode.F2;
             case 60: return KeyCode.F3;
@@ -89,9 +95,15 @@ public class SdlScanmap{
             case 67: return KeyCode.F10;
             case 68: return KeyCode.F11;
             case 69: return KeyCode.F12;
+            case 70: return KeyCode.PRINT_SCREEN;
+            case 71: return KeyCode.SCROLL_LOCK;
+            case 72: return KeyCode.PAUSE;
             case 73: return KeyCode.INSERT;
+            case 76: return KeyCode.DEL;
             case 74: return KeyCode.HOME;
             case 77: return KeyCode.END;
+            case 75: return KeyCode.PAGE_UP;
+            case 78: return KeyCode.PAGE_DOWN;
             case 79: return KeyCode.RIGHT;
             case 80: return KeyCode.LEFT;
             case 81: return KeyCode.DOWN;
