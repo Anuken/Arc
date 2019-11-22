5f1de5837d288008f8eee0827112d16488cd1b78
DeltaNedas <deltanedas@gmail.com>
2019-11-22 18:35:20 +0000

    Added reset keybind button to controls menu

--- arc-core/src/io/anuke/arc/scene/ui/KeybindDialog.java
+++ arc-core/src/io/anuke/arc/scene/ui/KeybindDialog.java
@@ -143,17 +143,24 @@ public class KeybindDialog extends Dialog{
                         rebindMin = true;
                         openDialog(section, keybind);
                     }).width(110f);
+                    table.addButton(bundle.get("settings.reset_key", "Reset"), () -> {
+                        resetKey(section, keybind);
+                    }).width(100f);
                     table.row();
                 }else{
                     table.add(bundle.get("keybind." + keybind.name() + ".name", Strings.capitalize(keybind.name())),
                     style.keyNameColor).left().padRight(40).padLeft(8);
                     table.add(keybinds.get(section, keybind).key.toString(),
                     style.keyColor).left().minWidth(90).padRight(20);
+
                     table.addButton(bundle.get("settings.rebind", "Rebind"), () -> {
                         rebindAxis = false;
                         rebindMin = false;
                         openDialog(section, keybind);
                     }).width(110f);
+                    table.addButton(bundle.get("settings.reset_key", "Reset"), () -> {
+                        resetKey(section, keybind);
+                    }).width(100f);
                     table.row();
                 }
             }
@@ -173,6 +180,19 @@ public class KeybindDialog extends Dialog{
 
         cont.add(pane).growX().colspan(sections.length);
 
+    }
+
+    private void resetKey(Section section, KeyBind bind){
+        String rname = "keybind-" + section.name + "-" + section.device.name() + "-" + bind.name();
+        settings.remove(rname + "-single");
+        settings.remove(rname + "-key");
+        settings.remove(rname + "-min");
+        settings.remove(rname + "-max");
+
+        settings.save();
+        rebindKey = null;
+        rebindAxis = false;
+        setup();
     }
 
     private void rebind(Section section, KeyBind bind, KeyCode newKey){
