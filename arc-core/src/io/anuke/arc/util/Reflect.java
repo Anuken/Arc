6ae7147787c9e35ef187fe28b5ec82b550458dae
Anuken <arnukren@gmail.com>
2019-12-17 11:46:28 -0500

    Added reflection utility class

---
+++ arc-core/src/io/anuke/arc/util/Reflect.java
@@ -0,0 +1,43 @@
+package io.anuke.arc.util;
+
+import java.lang.reflect.*;
+
+@SuppressWarnings("unchecked")
+public class Reflect{
+
+    public static <T> T get(Class<?> type, Object object, String name){
+        try{
+            Field field = type.getDeclaredField(name);
+            field.setAccessible(true);
+            return (T)field.get(object);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T get(Object object, String name){
+        return get(object.getClass(), object, name);
+    }
+
+    public static <T> T get(Class<?> type, String name){
+        return get(type, null, name);
+    }
+
+    public static void set(Class<?> type, Object object, String name, Object value){
+        try{
+            Field field = type.getDeclaredField(name);
+            field.setAccessible(true);
+            field.set(object, value);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void set(Object object, String name, Object value){
+        set(object.getClass(), object, name, value);
+    }
+
+    public static void set(Class<?> type, String name, Object value){
+        set(type, null, name, value);
+    }
+}
