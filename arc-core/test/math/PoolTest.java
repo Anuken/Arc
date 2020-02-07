0e6ac9406fa34f2f5596ff1b151a3ebd07856aac
Anuken <arnukren@gmail.com>
2020-02-06 20:24:30 -0500

    ?

---
+++ arc-core/test/math/PoolTest.java
@@ -0,0 +1,51 @@
+package math;
+
+import arc.func.*;
+import arc.struct.*;
+import arc.util.*;
+import arc.util.pooling.*;
+import org.junit.*;
+
+public class PoolTest{
+
+    @Test
+    public void allocation(){
+        long start = 0;
+
+        int objects = 100000;
+        Array<Object> list = new Array<>(objects);
+        Pools.get(Object.class, Object::new, objects);
+
+        for(int i = 0; i < objects; i++){
+            list.add(Pools.obtain(Object.class, Object::new));
+        }
+
+        Pools.freeAll(list, true);
+        list.clear();
+
+        long pre = memory();
+
+        for(int i = 0; i < objects; i++){
+            list.add(Pools.get(Object.class, Object::new).obtain());
+        }
+
+        Pools.freeAll(list, true);
+        list.clear();
+
+        for(int i = 0; i < objects; i++){
+            list.add(Pools.get(Object.class, Object::new).obtain());
+        }
+
+        long post = memory();
+
+        Prov a = Object::new;
+        Prov b = Object::new;
+        Log.info("a == b: {0}; codes: {1} {2}; equality: {3}", a == b, a.hashCode(), b.hashCode(), a.equals(b));
+        Log.info("Memory delta: {0} b", (post - pre));
+        Log.info("Total memory allocated: {0} mb", Strings.fixed((post - start)/1024f/1024f, 1));
+    }
+
+    long memory(){
+        return Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+    }
+}
