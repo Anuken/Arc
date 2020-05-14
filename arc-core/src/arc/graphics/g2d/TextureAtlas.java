1ccdb62216c9e6e0d85a85aa120f4dfc59b76e63
Anuken <arnukren@gmail.com>
2020-05-13 20:42:58 -0400

    goodbye

--- arc-core/src/arc/graphics/g2d/TextureAtlas.java
+++ arc-core/src/arc/graphics/g2d/TextureAtlas.java
@@ -245,21 +245,6 @@ public class TextureAtlas implements Disposable{
         return region == null || region == error ? def : region;
     }
 
-    /**
-     * Returns the first region found with the specified name and index. This method uses string comparison to find the region, so
-     * the result should be cached rather than calling this method multiple times.
-     * @return The region, or null.
-     */
-    public AtlasRegion find(String name, int index){
-        for(int i = 0, n = regions.size; i < n; i++){
-            AtlasRegion region = regions.get(i);
-            if(!region.name.equals(name)) continue;
-            if(region.index != index) continue;
-            return region;
-        }
-        return null;
-    }
-
     public boolean has(String s){
         return regionmap.containsKey(s);
     }
