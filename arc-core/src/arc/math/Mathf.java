56a811f5a4772c7e44881f4d73131c05737792cf
Anuken <arnukren@gmail.com>
2020-06-13 18:20:10 -0400

    Reconstructor fixes

--- arc-core/src/arc/math/Mathf.java
+++ arc-core/src/arc/math/Mathf.java
@@ -125,6 +125,10 @@ public final class Mathf{
         return y < 0f ? atan - PI : atan;
     }
 
+    public static int digits(int n){
+        return n < 100000 ? n < 100 ? n < 10 ? 1 : 2 : n < 1000 ? 3 : n < 10000 ? 4 : 5 : n < 10000000 ? n < 1000000 ? 6 : 7 : n < 100000000 ? 8 : n < 1000000000 ? 9 : 10;
+    }
+
     public static float sqrt(float x){
         return (float) Math.sqrt(x);
     }
