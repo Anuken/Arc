93182a28a728438ae456919ec0265afdb77d5090
Anuken <arnukren@gmail.com>
2020-08-08 18:01:54 -0400

    Sensor types

--- arc-core/src/arc/func/Cons2.java
+++ arc-core/src/arc/func/Cons2.java
@@ -2,11 +2,4 @@ package arc.func;
 
 public interface Cons2<T, N>{
     void get(T t, N n);
-
-    default Cons2<T, N> with(Cons2<T, N> cons){
-        return (t, n) -> {
-            get(t, n);
-            cons.get(t, n);
-        };
-    }
 }
