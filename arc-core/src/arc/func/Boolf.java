1040fdcbb5760e4c4a646d64782a25761efc1159
Anuken <arnukren@gmail.com>
2020-08-22 17:23:40 -0400

    No functional interface default methods

--- arc-core/src/arc/func/Boolf.java
+++ arc-core/src/arc/func/Boolf.java
@@ -2,12 +2,4 @@ package arc.func;
 
 public interface Boolf<T>{
     boolean get(T t);
-
-    default Boolf<T> and(Boolf<T> pred){
-        return t -> get(t) && pred.get(t);
-    }
-
-    default Boolf<T> or(Boolf<T> pred){
-        return t -> get(t) || pred.get(t);
-    }
 }
