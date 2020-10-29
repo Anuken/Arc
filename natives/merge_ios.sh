49a4e424cf844f6accc447c6eea7c86acec98039
Anuken <arnukren@gmail.com>
2020-10-29 11:37:05 -0400

    Libs

--- natives/merge_ios.sh
+++ natives/merge_ios.sh
@@ -1,5 +1,5 @@
-lipo lib$1.a.arm64 lib$1.a.arm7 lib$1.a.x86_64 lib$1.a.386 -create -output lib$1.a
-rm lib$1.a.arm64 lib$1.a.arm7 lib$1.a.x86_64 lib$1.a.386
+lipo lib$1.a.arm64 lib$1.a.armv7 lib$1.a.x86_64 lib$1.a.386 -create -output lib$1.a
+rm lib$1.a.arm64 lib$1.a.armv7 lib$1.a.x86_64 lib$1.a.386
 
 lipo lib$1.a.tvos.x86_64 lib$1.a.tvos.arm64 -create -output lib$1.a.tvos
 rm lib$1.a.tvos.x86_64 lib$1.a.tvos.arm64
