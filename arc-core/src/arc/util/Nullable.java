9f916a1b401216c68e39635f6c99769391c8555a
buthed010203 <naguiar010203@gmail.com>
2021-02-26 13:16:00 -0500

    Make Nullable Documented (#47)
    Before: ![](https://aethex.is-a.fail/FAcOxj.png)
    
    After: ![](https://i-dont.go-outsi.de/5nwOez.png)

--- arc-core/src/arc/util/Nullable.java
+++ arc-core/src/arc/util/Nullable.java
@@ -3,6 +3,7 @@ package arc.util;
 import java.lang.annotation.*;
 
 /** Indicates that a method return or field can be null. */
+@Documented
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Nullable{
