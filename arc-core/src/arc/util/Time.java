34b3882dd47595ac234cc0462bfb69ec1011c5b5
liuxilu <53636947+liuxilu@users.noreply.github.com>
2021-03-04 11:13:17 +0800

    Update Time.java: midnight is night...

--- arc-core/src/arc/util/Time.java
+++ arc-core/src/arc/util/Time.java
@@ -108,7 +108,7 @@ public class Time{
         return System.nanoTime();
     }
 
-    /** @return the difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC. */
+    /** @return the difference, measured in milliseconds, between the current time and the very start of January 1, 1970 UTC. */
     public static long millis(){
         return System.currentTimeMillis();
     }
