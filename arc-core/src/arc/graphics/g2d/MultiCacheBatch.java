e50e57951ca8b497a96ce7e1b46870479a416107
Anuken <arnukren@gmail.com>
2021-03-19 10:23:39 -0400

    Cleanup

--- arc-core/src/arc/graphics/g2d/MultiCacheBatch.java
+++ arc-core/src/arc/graphics/g2d/MultiCacheBatch.java
@@ -6,6 +6,7 @@ import arc.math.*;
 import arc.struct.*;
 import arc.util.*;
 
+//TODO this is an abomination that must be removed eventually
 public class MultiCacheBatch extends Batch{
     private static final int maxSpritesPerCache = 102000;
     Seq<SpriteCache> caches = new Seq<>();
