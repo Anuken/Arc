88c8e47fb9eaa9c9c3004b3b1b7e7167b0032aa4
Anuken <arnukren@gmail.com>
2021-05-06 12:04:15 -0400

    Removed java.awt from GifRecorder

--- extensions/recorder/src/arc/gif/GifRecorder.java
+++ extensions/recorder/src/arc/gif/GifRecorder.java
@@ -2,21 +2,14 @@ package arc.gif;
 
 import arc.*;
 import arc.files.*;
-import arc.graphics.Color;
+import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.input.*;
 import arc.math.*;
-import arc.math.geom.Rect;
 import arc.struct.*;
 import arc.util.*;
-import org.jcodec.api.awt.*;
-import org.jcodec.common.io.*;
-import org.jcodec.common.model.*;
+import arc.util.async.*;
 
-import javax.imageio.stream.*;
-import java.awt.*;
-import java.awt.datatransfer.*;
-import java.awt.image.*;
 import java.io.*;
 import java.text.*;
 import java.util.*;
@@ -24,8 +17,6 @@ import java.util.*;
 /** Records and saves GIFs. */
 public class GifRecorder{
     private static final float defaultSize = 300;
-    private static final boolean encodeMP4 = false;
-    private static BufferedImage outImage;
 
     public KeyCode
         resizeKey = KeyCode.controlLeft,
@@ -34,17 +25,16 @@ public class GifRecorder{
         shiftKey = KeyCode.shiftLeft;
 
     public Fi exportDirectory;
-    public boolean disableGUI, autoCopy = true;
+    public boolean disableGUI;
     public float speedMultiplier = 1f;
     public int recordfps = 30;
     public float driftSpeed = 1f;
     public float gifx, gify, gifwidth, gifheight;
+    public boolean recording, open, saving;
 
     private float offsetx, offsety;
     private Seq<byte[]> frames = new Seq<>();
     private float frametime;
-    private boolean recording, open;
-    private boolean saving;
     private float saveprogress;
 
     public GifRecorder(){
@@ -62,8 +52,9 @@ public class GifRecorder{
     protected void doInput(){
         if(Core.input.keyTap(openKey) && !saving){
             if(recording){
-                finishRecording();
-                clearFrames();
+                recording = false;
+                frames.clear();
+                recording = false;
             }
             open = !open;
         }
@@ -71,10 +62,49 @@ public class GifRecorder{
         if(open){
             if(Core.input.keyTap(recordKey) && !saving){
                 if(!recording){
-                    startRecording();
+                    frames.clear();
+                    recording = false;
+                    recording = true;
                 }else{
-                    finishRecording();
-                    writeGIF(exportDirectory);
+                    recording = false;
+
+                    saving = true;
+                    saveprogress = 0f;
+
+                    Threads.daemon(() -> {
+                        if(frames.size == 0){
+                            saving = false;
+                            return;
+                        }
+
+                        try{
+                            String time = "" + new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault()).format(new Date());
+                            exportDirectory.mkdirs();
+
+                            String args = Strings.format(
+                            "/usr/bin/ffmpeg -r @ -s @x@ -f rawvideo -pix_fmt rgba -i - -frames:v @ -filter:v vflip,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse @/@.gif",
+                            recordfps, (int)gifwidth, (int)gifheight, frames.size, exportDirectory.absolutePath(), time
+                            );
+
+                            ProcessBuilder builder = new ProcessBuilder(args.split(" ")).redirectErrorStream(true);
+                            Process process = builder.start();
+                            OutputStream out = process.getOutputStream();
+
+                            for(byte[] frame : frames){
+                                out.write(frame);
+                                out.flush();
+                                saveprogress += (1f / frames.size);
+                            }
+
+                            out.close();
+                            process.waitFor();
+                            frames.clear();
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+
+                        saving = false;
+                    });
                 }
             }
         }
@@ -97,7 +127,7 @@ public class GifRecorder{
 
         if(!disableGUI) Draw.color(Color.yellow);
 
-        if(Core.input.keyDown(resizeKey)){
+        if(Core.input.keyDown(resizeKey) && !recording){
 
             if(!disableGUI)
                 Draw.color(Color.green);
@@ -117,6 +147,18 @@ public class GifRecorder{
             float ys = (Core.graphics.getHeight() / 2 - Core.input.mouseY());
             offsetx = Mathf.lerpDelta(offsetx, -xs, driftSpeed);
             offsety = Mathf.lerpDelta(offsety, -ys, driftSpeed);
+        }
+
+        if(recording){
+            frametime += delta * 61f * speedMultiplier;
+            if(frametime >= (60f / recordfps)){
+                byte[] pix = ScreenUtils.getFrameBufferPixels(
+                (int)(gifx + offsetx + Core.graphics.getWidth() / 2),
+                (int)(gify + offsety + Core.graphics.getHeight() / 2),
+                (int)(gifwidth), (int)(gifheight), false);
+                frames.add(pix);
+                frametime = 0;
+            }
         }
 
         if(!disableGUI){
@@ -149,163 +191,9 @@ public class GifRecorder{
             Draw.color(Color.white);
         }
 
-        if(recording){
-            frametime += delta * 61f * speedMultiplier;
-            if(frametime >= (60f / recordfps)){
-                byte[] pix = ScreenUtils.getFrameBufferPixels(
-                    (int)(gifx + offsetx) + 1 + Core.graphics.getWidth() / 2,
-                    (int)(gify + offsety) + 1 + Core.graphics.getHeight() / 2,
-                    (int)(gifwidth) - 2, (int)(gifheight) - 2, false);
-                frames.add(pix);
-                frametime = 0;
-            }
-        }
-
         Draw.color();
         Draw.flush();
         Draw.proj(Tmp.m1);
     }
 
-    public boolean isSaving(){
-        return saving;
-    }
-
-    public boolean isOpen(){
-        return open;
-    }
-
-    public void open(){
-        open = true;
-    }
-
-    public void close(){
-        open = false;
-    }
-
-    public boolean isRecording(){
-        return recording;
-    }
-
-    public void startRecording(){
-        clearFrames();
-        recording = true;
-    }
-
-    public void finishRecording(){
-        recording = false;
-    }
-
-    public void clearFrames(){
-        frames.clear();
-        recording = false;
-    }
-
-    /** Sets the bounds for recording, relative to the center of the screen */
-    public void setBounds(float x, float y, float width, float height){
-        this.gifx = x;
-        this.gify = y;
-        this.gifwidth = width;
-        this.gifheight = height;
-    }
-
-    public void setBounds(Rect rect){
-        setBounds(rect.x, rect.y, rect.width, rect.height);
-    }
-
-    private void writeGIF(final Fi writedirectory){
-        if(saving) return;
-        saving = true;
-
-        int width = (int)(gifwidth) - 2, height = (int)(gifheight) - 2;
-        saveprogress = 0f;
-
-        new Thread(() -> {
-            File f = compileGIF(frames, width, height, writedirectory);
-
-            //copy image to clipboard
-            if(autoCopy){
-                Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new Transferable(){
-                    @Override
-                    public DataFlavor[] getTransferDataFlavors(){
-                        return new DataFlavor[]{DataFlavor.javaFileListFlavor};
-                    }
-
-                    @Override
-                    public boolean isDataFlavorSupported(DataFlavor flavor){
-                        return DataFlavor.javaFileListFlavor.equals(flavor);
-                    }
-
-                    @Override
-                    public Object getTransferData(DataFlavor flavor){
-                        return Collections.singletonList(f);
-                    }
-                }, (clipboard, contents) -> {});
-            }
-
-            saving = false;
-        }).start();
-    }
-
-    private File compileGIF(Seq<byte[]> pixmaps, int width, int height, Fi directory){
-        if(pixmaps.size == 0){
-            throw new RuntimeException("No input files!");
-        }
-
-        try{
-            String time = "" + new SimpleDateFormat("yyyy-MM-dd HH-mm-ss", Locale.getDefault()).format(new Date());
-            new File(directory.absolutePath()).mkdir();
-
-            File file = new File(directory.absolutePath() + "/" + time + ".gif");
-
-            if(encodeMP4){
-                try(SeekableByteChannel out = NIOUtils.writableFileChannel(directory.absolutePath() + "/" + time + ".mp4")){
-                    AWTSequenceEncoder encoder = new AWTSequenceEncoder(out, Rational.R(recordfps, 1));
-
-                    for(byte[] pixmap : pixmaps){
-                        encoder.encodeImage(toImage(pixmap, width, height));
-                        saveprogress += (1f / frames.size);
-                    }
-
-                    encoder.finish();
-                }
-            }else{
-                ImageOutputStream output = new FileImageOutputStream(file);
-                GifSequenceWriter writer = new GifSequenceWriter(output, BufferedImage.TYPE_INT_ARGB, (int)(1f / recordfps * 1000f), true);
-
-                for(int i = 0; i < pixmaps.size; i++){
-                    writer.write(toImage(pixmaps.get(i), width, height));
-                    saveprogress += (1f / frames.size);
-                }
-                writer.close();
-                output.close();
-            }
-            pixmaps.clear();
-
-            return file;
-        }catch(Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private BufferedImage toImage(byte[] frames, int width, int height){
-        if(outImage == null || outImage.getWidth() != width || outImage.getHeight() != height){
-            outImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        }
-        BufferedImage image = outImage;
-
-        for(int i = 0; i < width * height * 4; i += 4){
-            int r = frames[i];
-            int g = frames[i + 1];
-            int b = frames[i + 2];
-            if(r < 0) r += 256;
-            if(g < 0) g += 256;
-            if(b < 0) b += 256;
-
-            int result = Color.argb8888(1f, r / 255f, g / 255f, b / 255f);
-            int index = i / 4;
-            image.setRGB(index % width, height - 1 - index / width, result);
-        }
-        return image;
-    }
 }
