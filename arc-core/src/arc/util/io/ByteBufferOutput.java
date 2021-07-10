4b1c4b4565f24298c9c5c7e9b40d94957d4aa226
Anuken <arnukren@gmail.com>
2021-07-10 18:53:51 -0400

    Fixed ByteBufferInput/Output UTF methods

--- arc-core/src/arc/util/io/ByteBufferOutput.java
+++ arc-core/src/arc/util/io/ByteBufferOutput.java
@@ -1,8 +1,7 @@
 package arc.util.io;
 
-import java.io.DataOutput;
-import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
+import java.io.*;
+import java.nio.*;
 
 /** DataOutput wrapper of ByteBuffer. */
 public class ByteBufferOutput implements DataOutput{
@@ -83,14 +82,47 @@ public class ByteBufferOutput implements DataOutput{
     }
 
     @Override
-    public void writeUTF(String s){
-        try{
-            byte[] bytes = s.getBytes("UTF-8");
-            if(bytes.length >= Short.MAX_VALUE) throw new IllegalArgumentException("Input string is too long!");
-            buffer.putShort((short)bytes.length);
-            buffer.put(bytes);
-        }catch(UnsupportedEncodingException e){
-            throw new RuntimeException(e);
+    public void writeUTF(String s) throws IOException{
+        final int strlen = s.length();
+        int utflen = strlen; // optimized for ASCII
+
+        for(int i = 0; i < strlen; i++){
+            int c = s.charAt(i);
+            if(c >= 0x80 || c == 0)
+                utflen += (c >= 0x800) ? 2 : 1;
         }
+
+        if(utflen > 65535 || /* overflow */ utflen < strlen)
+            throw new UTFDataFormatException("encoded string too long");
+
+        final byte[] bytearr;
+        bytearr = new byte[utflen + 2];
+
+        int count = 0;
+        bytearr[count++] = (byte)((utflen >>> 8) & 0xFF);
+        bytearr[count++] = (byte)((utflen >>> 0) & 0xFF);
+
+        int i = 0;
+        for(i = 0; i < strlen; i++){ // optimized for initial run of ASCII
+            int c = s.charAt(i);
+            if(c >= 0x80 || c == 0) break;
+            bytearr[count++] = (byte)c;
+        }
+
+        for(; i < strlen; i++){
+            int c = s.charAt(i);
+            if(c < 0x80 && c != 0){
+                bytearr[count++] = (byte)c;
+            }else if(c >= 0x800){
+                bytearr[count++] = (byte)(0xE0 | ((c >> 12) & 0x0F));
+                bytearr[count++] = (byte)(0x80 | ((c >> 6) & 0x3F));
+                bytearr[count++] = (byte)(0x80 | ((c >> 0) & 0x3F));
+            }else{
+                bytearr[count++] = (byte)(0xC0 | ((c >> 6) & 0x1F));
+                bytearr[count++] = (byte)(0x80 | ((c >> 0) & 0x3F));
+            }
+        }
+        write(bytearr, 0, utflen + 2);
     }
+
 }
