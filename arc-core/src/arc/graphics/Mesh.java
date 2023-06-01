791c5fa5022c1283574ce95f9ce99df5ce559987
Anuken <arnukren@gmail.com>
2023-06-01 09:07:17 -0400

    Nobody uses these chips (https://en.wikipedia.org/wiki/MSM7000)

--- arc-core/src/arc/graphics/Mesh.java
+++ arc-core/src/arc/graphics/Mesh.java
@@ -226,9 +226,7 @@ public class Mesh implements Disposable{
 
     /**
      * Sets whether to bind the underlying {@link VertexArray} or {@link VertexBufferObject} automatically on a call to one of the
-     * render methods. Usually you want to use autobind. Manual binding is an expert functionality. There is a driver bug on the
-     * MSM720xa chips that will fuck up memory if you manipulate the vertices and indices of a Mesh multiple times while it is
-     * bound. Keep this in mind.
+     * render methods. Usually you want to use autobind. Manual binding is an expert functionality.
      * @param autoBind whether to autobind meshes.
      */
     public void setAutoBind(boolean autoBind){
