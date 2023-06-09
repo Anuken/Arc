708a2a30855d472cd785c88dccef7032df323bd3
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2023-06-09 20:11:06 +0200

    enable VT100 in windows for colored text
    all is explain in file changes

--- arc-core/src/arc/util/ColorCodes.java
+++ arc-core/src/arc/util/ColorCodes.java
@@ -36,9 +36,61 @@ public class ColorCodes{
     public static final String[] codes, values;
 
     static{
+        byte exitCode = 0;
+        
+        if(OS.isWindows){
+            /* Enable VT100, ANSI escapes codes, for windows consoles
+            * 
+            * Run a Window script to add a key in registry of user (no admin permissions needed) 
+            * to enable the VT100 feature.
+            * This is only, simple, way i found to do colored text with ANSI codes, without use 
+            * an library like jna to run kernel32.GetStdHandle and kernel32.SetConsoleMode methods.
+            * The problem (or not), this change is permanent and affect all windows consoles 
+            * (cmd, powershell, ...) of user, and need to restart the console to take effect.
+            * 
+            * Exit codes:
+            *   - 0: key added, need restart (so disable colors for this instance)
+            *   - 1: error while adding key (probably user group strategy block this), quiet rare
+            *   - 2: key already added (VT100 already enabled, so using colors)
+            */ 
 
-        //disable color codes on windows/android (ignore windows terminal which supports colors)
-        if((OS.isWindows && !OS.hasEnv("WT_SESSION")) || OS.isAndroid){
+            try{
+                String command1 = "reg query HKCU\\Console /v VirtualTerminalLevel",
+                    command2 = "reg add HKCU\\Console /f /v VirtualTerminalLevel /t REG_DWORD /d 1";
+
+                Process process = new ProcessBuilder(command1.split(" ")).redirectErrorStream(true).start();
+                
+                if(process.waitFor() != 0){
+                    // Key don't exist, so create it
+                    process = new ProcessBuilder(command2.split(" ")).redirectErrorStream(true).start();
+                    exitCode = process.waitFor();
+                
+                }else{
+                    // Key exist, so check if value is at 0x1 or more
+                    BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    bool goodValue = false;
+
+                    for(String line : in.lines().toList()){
+                        if(line.contains("VirtualTerminalLevel")){
+                            goodValue = Integer.decode(line.substring(line.lastIndexOf("0x")).replace("\n", "").trim()) > 0;
+                            if(goodValue) break; // Value is 1 or more
+                        }
+                    }
+
+                    if(goodValue){
+                        exitCode = 2;
+
+                    }else{
+                        // Value is 0 so change this at 1;
+                        process = new ProcessBuilder(command2.split(" ")).redirectErrorStream(true).start();
+                        exitCode = process.waitFor();
+                    }
+                }
+
+            }catch(java.io.IOException | InterruptedException e){} // Error while running commands, don't enable colors
+        }
+        
+        if(OS.isAndroid || (OS.isWindows && exitCode != 2)){
             flush = reset = bold = underline = black = red = green = yellow = blue = purple = cyan = lightWhite
             = lightBlack = lightRed = lightGreen = lightYellow = lightBlue = lightMagenta = lightCyan
             = white = backDefault = backRed = backYellow = backBlue = backGreen = italic = "";
