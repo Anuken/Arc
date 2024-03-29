e312cc96f5c044df9235b2d68b592720171cc215
Anuken <arnukren@gmail.com>
2024-03-28 22:04:09 -0400

    Optimizations for GL30

--- arc-core/src/arc/graphics/g2d/SpriteBatch.java
+++ arc-core/src/arc/graphics/g2d/SpriteBatch.java
@@ -78,6 +78,9 @@ public class SpriteBatch extends Batch{
             }else{
                 shader = defaultShader;
             }
+
+            //mark indices as dirty once for GL30
+            mesh.getIndicesBuffer();
         }else{
             vertices = new float[0];
             shader = null;
@@ -106,8 +109,6 @@ public class SpriteBatch extends Batch{
         lastTexture.bind();
         Mesh mesh = this.mesh;
         mesh.setVertices(vertices, 0, idx);
-        mesh.getIndicesBuffer().position(0);
-        mesh.getIndicesBuffer().limit(count);
         mesh.render(getShader(), Gl.triangles, 0, count);
 
         idx = 0;
