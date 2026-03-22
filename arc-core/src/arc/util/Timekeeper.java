f9072fcc02b4801caf80a116081a666ae0b5fa1d
Anuken <arnukren@gmail.com>
2026-03-22 12:30:16 -0400

    Timekeeper improvements

--- arc-core/src/arc/util/Timekeeper.java
+++ arc-core/src/arc/util/Timekeeper.java
@@ -1,18 +1,40 @@
 package arc.util;
 
+/** Keeps track of a time interval. */
 public class Timekeeper{
-    private final long intervalms;
-    private long time;
+    private final long intervalMs;
+    private long lastTime;
 
-    public Timekeeper(float seconds){
-        intervalms = (int)(seconds * 1000);
+    Timekeeper(long ms){
+        intervalMs = ms;
     }
 
+    public static Timekeeper ofMillis(long ms){
+        return new Timekeeper(ms);
+    }
+
+    public static Timekeeper ofTicks(float ticks){
+        return ofSeconds(ticks / 60f);
+    }
+
+    public static Timekeeper ofSeconds(float seconds){
+        return new Timekeeper((long)(seconds * 1000));
+    }
+
+    /** @return true if the interval has passed since the last reset(); resets the timer if true */
+    public boolean poll(){
+        boolean result = get();
+        if(result) reset();
+        return result;
+    }
+
+    /** @return true if the interval has passed since the last reset(). */
     public boolean get(){
-        return Time.timeSinceMillis(time) > intervalms;
+        return Time.timeSinceMillis(lastTime) > intervalMs;
     }
 
+    /** resets the timer; the interval will need to pass until get() returns true again. */
     public void reset(){
-        time = Time.millis();
+        lastTime = Time.millis();
     }
 }
--- arc-core/src/arc/util/io/Reads.java
+++ arc-core/src/arc/util/io/Reads.java
@@ -1,9 +1,14 @@
 package arc.util.io;
 
+import arc.util.*;
+
 import java.io.*;
 
 /** A wrapper for DataInput with more concise method names and no IOExceptions. */
 public class Reads implements Closeable{
+    private @Nullable byte[] bytearr;
+    private @Nullable char[] chararr;
+
     public DataInput input;
 
     public Reads(DataInput input){
@@ -153,6 +158,91 @@ public class Reads implements Closeable{
     public void skip(int amount){
         try{
             input.skipBytes(amount);
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** read string (UTF), with an optional max length */
+    public String str(int maxLen){
+        try{
+            DataInput in = input;
+
+            int utflen = in.readUnsignedShort();
+            if(maxLen > 0 && utflen > maxLen) throw new RuntimeException("String too long: " + maxLen);
+
+            if(bytearr == null || bytearr.length < utflen){
+                bytearr = new byte[utflen * 2];
+                chararr = new char[utflen * 2];
+            }
+
+            int count = 0;
+            int chararr_count = 0;
+            in.readFully(bytearr, 0, utflen);
+
+            int c;
+            while(count < utflen){
+                c = bytearr[count] & 255;
+                if(c > 127){
+                    break;
+                }
+
+                ++count;
+                chararr[chararr_count++] = (char)c;
+            }
+
+            while(count < utflen){
+                c = bytearr[count] & 255;
+                byte char2;
+                switch(c >> 4){
+                    case 0:
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                        ++count;
+                        chararr[chararr_count++] = (char)c;
+                        break;
+                    case 8:
+                    case 9:
+                    case 10:
+                    case 11:
+                    default:
+                        throw new UTFDataFormatException("malformed input around byte " + count);
+                    case 12:
+                    case 13:
+                        count += 2;
+                        if(count > utflen){
+                            throw new UTFDataFormatException("malformed input: partial character at end");
+                        }
+
+                        char2 = bytearr[count - 1];
+                        if((char2 & 192) != 128){
+                            throw new UTFDataFormatException("malformed input around byte " + count);
+                        }
+
+                        chararr[chararr_count++] = (char)((c & 31) << 6 | char2 & 63);
+                        break;
+                    case 14:
+                        count += 3;
+                        if(count > utflen){
+                            throw new UTFDataFormatException("malformed input: partial character at end");
+                        }
+
+                        char2 = bytearr[count - 2];
+                        int char3 = bytearr[count - 1];
+                        if((char2 & 192) != 128 || (char3 & 192) != 128){
+                            throw new UTFDataFormatException("malformed input around byte " + (count - 1));
+                        }
+
+                        chararr[chararr_count++] = (char)((c & 15) << 12 | (char2 & 63) << 6 | (char3 & 63) << 0);
+                }
+            }
+
+            return new String(chararr, 0, chararr_count);
         }catch(IOException e){
             throw new RuntimeException(e);
         }
