65a907e76bdcb43b7bbf3c8a356f3c272bf31a01
Anuken <arnukren@gmail.com>
2026-04-10 18:33:02 -0400

    Testing if calling order matters

--- backends/backend-sdl/src/arc/backend/sdl/SdlApplication.java
+++ backends/backend-sdl/src/arc/backend/sdl/SdlApplication.java
@@ -112,10 +112,10 @@ public class SdlApplication implements Application{
 
         if(OS.isMac) restartMac();
 
-        //show native IME candidate UI
-        SDL_SetHint("SDL_IME_SHOW_UI","1");
-
         check(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS));
+
+        SDL_SetHint("SDL_IME_SHOW_UI","1");
+        SDL_SetHint("SDL_WINDOWS_DPI_SCALING", "1");
 
         check(SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, OS.isMac || config.coreProfile ? SDL_GL_CONTEXT_PROFILE_CORE : SDL.SDL_GL_CONTEXT_PROFILE_COMPATIBILITY));
 
