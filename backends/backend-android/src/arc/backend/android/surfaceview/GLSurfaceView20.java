c2e2d470c8c0a40277a6baa7af609825fe1b956a
Anuken <arnukren@gmail.com>
2026-07-30 15:46:51 -0400

    MinAPI Android cleanup

--- backends/backend-android/src/arc/backend/android/surfaceview/GLSurfaceView20.java
+++ backends/backend-android/src/arc/backend/android/surfaceview/GLSurfaceView20.java
@@ -13,22 +13,18 @@
 
 package arc.backend.android.surfaceview;
 
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.graphics.PixelFormat;
-import android.opengl.GLSurfaceView;
-import android.os.SystemClock;
-import android.util.Log;
-import android.view.KeyCharacterMap;
-import android.view.KeyEvent;
-import android.view.inputmethod.BaseInputConnection;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputConnection;
+import android.content.*;
+import android.graphics.*;
+import android.opengl.*;
+import android.os.*;
+import android.util.*;
+import android.view.*;
+import android.view.inputmethod.*;
 
-import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLContext;
 import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.*;
 
 /**
  * A simple GLSurfaceView sub-class that demonstrates how to perform OpenGL ES 2.0 rendering into a GL Surface. Note the following
@@ -94,21 +90,13 @@ public class GLSurfaceView20 extends GLSurfaceView{
         return new BaseInputConnection(GLSurfaceView20.this, false){
             @Override
             public boolean deleteSurroundingText(int beforeLength, int afterLength){
-                int sdkVersion = android.os.Build.VERSION.SDK_INT;
-                if(sdkVersion >= 16){
-                    /*
-                     * In Jelly Bean, they don't send key events for delete. Instead, they send beforeLength = 1, afterLength = 0. So,
-                     * we'll just simulate what it used to do.
-                     */
-                    if(beforeLength == 1 && afterLength == 0){
-                        sendDownUpKeyEventForBackwardCompatibility(KeyEvent.KEYCODE_DEL);
-                        return true;
-                    }
+                if(beforeLength == 1 && afterLength == 0){
+                    sendDownUpKeyEventForBackwardCompatibility(KeyEvent.KEYCODE_DEL);
+                    return true;
                 }
                 return super.deleteSurroundingText(beforeLength, afterLength);
             }
 
-            @TargetApi(16)
             private void sendDownUpKeyEventForBackwardCompatibility(final int code){
                 final long eventTime = SystemClock.uptimeMillis();
                 super.sendKeyEvent(new KeyEvent(eventTime, eventTime, KeyEvent.ACTION_DOWN, code, 0, 0,
