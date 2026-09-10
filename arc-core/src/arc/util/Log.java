c5026df794a952f7f97ed04dca3d9a03b0b84c59
phinner <62483793+Phinner@users.noreply.github.com>
2026-09-10 20:19:04 +0200

    sneaky space

--- arc-core/src/arc/util/Log.java
+++ arc-core/src/arc/util/Log.java
@@ -143,7 +143,7 @@ public class Log{
     public static class NoopLogHandler implements LogHandler{
         @Override public void log(LogLevel level, String text){}
         @Override public void log(LogLevel level, String tag, String text, Object... args){}
-        @Override public void logList(LogLevel level, String tag, Object... args) {}
+        @Override public void logList(LogLevel level, String tag, Object... args){}
         @Override public void logException(LogLevel level, String tag, String text, Throwable th){}
     }
 }
